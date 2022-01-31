505db98fbfecca5ea3fe02096702a04f3e583fdb
Ayush Nema <ayush.nema@toyotaconnected.co.in>
2022-01-31 22:55:42 +0530

    refinement of features

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from transformers import MarianTokenizer, AutoModelForSeq2SeqLM
 from utilities.utils import init_cmd_args, config_logger
 from source.translator import translate_text
 
-# $ python3 main.py -d ./data/qrg_smoke_new.csv -t en-es
-
 __author__ = "ayush@gyandata.com"
+
+# $ python3 main.py -d ./data/qrg_smoke_new.csv -t en-ro -o ./data/output/romanian-ro
+# $ python3 main.py --isdir  -d ./data/data_copy/ -t en-sk -o ./data/output/slovak-sk
 
 LOGGER = logging.getLogger(__name__)
 
@@ -40,6 +41,7 @@ translate_func = partial(translate_text, tokenizer=tokenizer, model=model)
 
 
 def _base_module(file_path):
+    start_time = time.perf_counter()
     # Reading data and performing translation
     df = pd.read_csv(file_path)
     LOGGER.info(f'Initiating translation for {os.path.basename(file_path)} --> length={df.__len__()}')
@@ -52,17 +54,17 @@ def _base_module(file_path):
 
     # Curating the information
     total_time = time.perf_counter() - start_time
-    LOGGER.info(f"{os.path.basename(file_path)} translated in {total_time:.3f}")
-    LOGGER.info(f"Approximate throughput for {os.path.basename(file_path)} ~ {total_time / df.shape[0]:0.3f} per sec")
+    LOGGER.info(f"{os.path.basename(file_path)} translated in {total_time:.3f} sec")
+    LOGGER.info(f"Approximate throughput for {os.path.basename(file_path)} ≅ {total_time / df.shape[0]:0.3f}/sec")
 
-
-start_time = time.perf_counter()
 
 if cmd_args.isdir:
     # If directory path is provided
     input_path = f'{cmd_args.input_path}/*.csv'
-    for i in glob.glob(input_path):
-        _base_module(file_path=i)
+    for idx, val in enumerate(glob.glob(input_path)):
+        _base_module(file_path=val)
+        LOGGER.info(f"-------------- {idx + 1}/{len(os.listdir(cmd_args.input_path))} "
+                    f"({(idx + 1)/(len(os.listdir(cmd_args.input_path)))*100:.2f}%) completed --------------")
 else:
     # If file path is provided
     _base_module(file_path=cmd_args.input_path)
